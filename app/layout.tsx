efc53ff5604482501c6a418afbc624e091aeaf23
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-06-21 12:20:39 +0600

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GitHub: Let's build from here · GitHub",
+  description:
+    "Whether you're scaling your startup or just learning how to code, GitHub is your home. Join the world's largest developer platform to build the innovations that",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
